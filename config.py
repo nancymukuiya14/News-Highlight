5f9cd22f9441e1dc4c6d6af5f3b035a148c74c8d
nancy.kigotho@student.moringaschool.com <nancykigotho14@gmail.com>
2021-09-10 21:17:37 +0300

    add the main folder

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?apiKey={}'
 NEWS_APIKEY = 'd0054354f7a3447581e739eb8be8abd4'
 NEWS_SECRET_KEY = os.urandom(32)
 BASE_URL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey={}'
-ARTICLE = 'https://newsapi.org/v2/everything?sources={}&apiKey={}'
+ARTICLE_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey={}'
 
 staticmethod
 def init_app():
