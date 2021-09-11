5ce1b56bab8ab2d9d6ec0cfeffa7659343626751
nancy.kigotho@student.moringaschool.com <nancykigotho14@gmail.com>
2021-09-11 20:45:22 +0300

    modify my request.py

--- app/request.py
+++ app/request.py
@@ -79,36 +79,36 @@ def process_article(article_list):
     return article_response
 
 
-def get_articles_category(category):
-    '''
-    Function that gets the json response to our url request
-    '''
-    get_articles_url = 'https://newsapi.org/v2/top-headlines?category={}&apiKey={}'.format(
-        category, api_key)
-    # get_articles_url = base_url_articles.format(source_id, api_key)
-    with urllib.request.urlopen(get_articles_url) as url:
-        get_articles_data = url.read()
-        # load the data into a json object
-        get_articles_response = json.loads(get_articles_data)
+# def get_articles_category(category):
+#     '''
+#     Function that gets the json response to our url request
+#     '''
+#     get_articles_url = 'https://newsapi.org/v2/top-headlines?category={}&apiKey={}'.format(
+#         category, api_key)
+#     # get_articles_url = base_url_articles.format(source_id, api_key)
+#     with urllib.request.urlopen(get_articles_url) as url:
+#         get_articles_data = url.read()
+#         # load the data into a json object
+#         get_articles_response = json.loads(get_articles_data)
 
-        articles_results = None
+#         articles_results = None
 
-        if get_articles_response['articles']:
-            articles_results_list = get_articles_response['articles']
-            articles_results = process_results_articles(articles_results_list)
-    return articles_results  # return the results
+#         if get_articles_response['articles']:
+#             articles_results_list = get_articles_response['articles']
+#             articles_results = process_results_articles(articles_results_list)
+#     return articles_results  # return the results
 
-def process_results_articles (articles_results_list):
-    articles_results = []
-    for article in articles_results_list:
-        id = article.get('id')
-        author = article.get('author')
-        title = article.get('title')
-        description = article.get('description')
-        url = article.get('url')
-        urlToImage = article.get('urlToImage')
-        publishedAt = article.get('piblishedAt')
-        if urlToImage:
-            article_object = Articles(id, author,title, description,url,urlToImage,publishedAt)
-            articles_results.append(article_object)
-    return articles_results
+# def process_results_articles (articles_results_list):
+#     articles_results = []
+#     for article in articles_results_list:
+#         id = article.get('id')
+#         author = article.get('author')
+#         title = article.get('title')
+#         description = article.get('description')
+#         url = article.get('url')
+#         urlToImage = article.get('urlToImage')
+#         publishedAt = article.get('piblishedAt')
+#         if urlToImage:
+#             article_object = Articles(id, author,title, description,url,urlToImage,publishedAt)
+#             articles_results.append(article_object)
+#     return articles_results
