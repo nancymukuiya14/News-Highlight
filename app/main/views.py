c204e57eb1060bebd5b3dc1340243a425cc9f7cb
nancy.kigotho@student.moringaschool.com <nancykigotho14@gmail.com>
2021-09-11 22:59:14 +0300

    style my cards

--- app/main/views.py
+++ app/main/views.py
@@ -1,12 +1,14 @@
+from app.models import Articles
 from flask import render_template
 from . import main
-from ..request import get_news_source, get_article
+from ..request import get_articles_category, get_news_source, get_article
 
 
 @main.route('/')
 def index():
     results = get_news_source()
-    return render_template('index.html', results = results)
+    articles = get_articles_category('entertainment') 
+    return render_template('index.html', results = results, articles = articles)
 
 @main.route('/news/<id>')
 def news(id):
@@ -14,3 +16,8 @@ def news(id):
     print(article)
     return render_template('article.html',article =article)
 
+@main.route('/category')
+def category(category):
+    article = get_articles_category(category)
+    print(article)
+    return render_template('category.html')
